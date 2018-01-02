abacfaa19926a3c42d15e0fcf152a2231e9b641f
jakubzavrel <jakubzavrel@seznam.cz>
2018-01-02 16:16:29 +0100

    footer and toggler styling

--- src/app/components/main-menu.js
+++ src/app/components/main-menu.js
@@ -2,13 +2,14 @@ import arrays from '../utilities/arrays';
 import events from '../utilities/events';
 
 const mainMenu = (container) => {
+    const menu = arrays.fromNodes(container.getElementsByClassName('js-menu'));
     const menuToggler = arrays.fromNodes(container.getElementsByClassName('js-menu-toggle'));
 
     events.on(menuToggler[0], { click: event => toggleMenu(event) });
 
     function toggleMenu(event) {
         event.preventDefault();
-        menuToggler[0].classList.toggle('active');
+        menu[0].classList.toggle('active');
     }
 };
 
